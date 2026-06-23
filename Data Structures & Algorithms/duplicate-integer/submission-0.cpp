df4fc50672183470707182a727cef0380d38ae63
LeelaSravanthi18 <22P31A04E5@acet.ac.in>
2026-06-23 15:36:49 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int>freq;
+        for(int i:nums)
+        {
+            freq[i]++;
+        }
+        for(auto it:freq)
+        {
+        if(it.second > 1)
+        {
+            return true;
+        }
+        }
+        return false;
+    }
+};
